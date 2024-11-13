a9118909ab82173289f65538b7b93eaf3da0d7c6
AnthonySchuijlenburg <a.schuijlenburg@gmail.com>
2024-11-13 12:11:27 +0100

    build: speed up tests by removing slow redundant tests

--- src/2015/04/StockingStuffers.test.ts
+++ src/2015/04/StockingStuffers.test.ts
@@ -2,16 +2,6 @@ import { describe, it, expect } from 'vitest';
 import { partOne, partTwo } from './StockingStuffers.ts';
 
 describe('StockingStuffers:Part one', () => {
-    it('example input 1', () => {
-        const result = partOne('abcdef');
-        expect(result).toBe(609043);
-    });
-
-    it('example input 2', () => {
-        const result = partOne('pqrstuv');
-        expect(result).toBe(1048970);
-    });
-
     it('fast input 1', () => {
         // input found by looking for the input that would result in the lowest number of hashes
         const result = partOne('ajte');
